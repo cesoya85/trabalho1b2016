4ee7f17fde959a26d2615eb94039d3628f2099e3
Carlos Estevan de Souza Oya <girafaoo@gmail.com>
2016-03-20 03:18:57 -0300

    Criando final classe de letra M

---
+++ src/br/univel/interfaces/ExM.java
@@ -0,0 +1,25 @@
+package br.univel.interfaces;
+
+class ExM implements Desenho {
+
+	public static int SIZE = 8;
+		
+	@Override
+	public void desenhar() {
+				
+		System.out.println("=====================================");
+		System.out.println("\n\nm)\n\n");
+				
+		for (int linha = 1; linha <= SIZE; linha++) {
+			for (int col = 1; col <= SIZE; col++) {
+				if (col > linha) {
+					System.out.print(" ");
+				} else {
+					System.out.print(col + " ");
+				}
+			}
+			System.out.print("\n");
+		}
+	}
+
+}
